67f3f45755d0ef3afd89ba22cba2f8fd5cee9d26
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-11 23:38:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ def tap(x, y):
 def inside(xy):
     "Return True if xy within screen."
     return -200 < xy.x < 200 and -200 < xy.y < 200
+
+def draw():
+    "Draw ball and targets."
+    clear()
+    for target in targets:
+        goto(target.x, target.y)
+        dot(20, 'blue')
+
+    if inside(ball):
+        goto(ball.x, ball.y)
+        dot(6, 'red')
+
+    update()
+
