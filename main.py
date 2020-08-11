39a14cda48da8f3707a063002930d7769a2377fb
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-11 23:35:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from random import randrange
+from turtle import *
+from freegames import vector
+
+ball = vector(-200, -200)
+speed = vector(0, 0)
+targets = []
+
+def tap(x, y):
+    "Respond to screen tap."
+    if not inside(ball):
+        ball.x = -199
+        ball.y = -199
+        speed.x = (x + 200)/25
+        speed.y = (y + 200)/25
+
+def inside(xy):
+    "Return True if xy within screen."
+    return -200 < xy.x < 200 and -200 < xy.y < 200
